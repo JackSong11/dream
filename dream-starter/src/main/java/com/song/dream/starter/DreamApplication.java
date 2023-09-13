2cd4416cfcf62b117e3311352021cd133a9a188e
songzhiquana <songzhiquana@enn.cn>
2023-09-13 18:00:36 +0800

    启动类

---
+++ dream-starter/src/main/java/com/song/dream/starter/DreamApplication.java
@@ -0,0 +1,19 @@
+package com.song.dream.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName DreamApplication
+ * @Description
+ * @Author songzhiquana
+ * @Date 17:57 2023/9/13
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class DreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamApplication.class, args);
+    }
+}
